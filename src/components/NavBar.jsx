3b9060a05c92a1a7644ad09d8c885e0d7a3f75e5
Odohofre <odohofrebright@gmail.com>
2023-03-11 14:07:17 +0100

    make navbar responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Button from "./Button";
+import open from "../assets/icon-menu.svg";
+import close from "../assets/icon-menu-close.svg";
 
 const links = [
   { id: "services", text: "services" },
@@ -9,13 +11,34 @@ const links = [
 
 export default function NavBar() {
   const [activeSection, setActiveSection] = useState("");
+  const [isOpen, setOpen] = useState(false);
+  const menuRef = useRef();
+
   function handleClick(sectionId) {
     setActiveSection(sectionId);
   }
 
+  const handleLinkClick = () => {
+    setOpen(false);
+  };
+
+  const handleClickOutside = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      handleLinkClick();
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, []);
+
   return (
     <header className="sticky top-0 z-50  bg-dark-800 text-white pt-8 pb-6">
-      <nav className="flex w-full items-center justify-between font-bold lg:px-30 uppercase">
+      <nav className="flex md:w-full items-center justify-between font-bold px-5 lg:px-30 uppercase">
         {/* Logo */}
         <div className="">
           <a
@@ -29,8 +52,8 @@ export default function NavBar() {
         </div>
 
         {/* DESKTOP NAV */}
-        <div className="flex items-center justify-between">
-          <div className="text-sm space-x-24">
+        <div className="hidden md:flex md:items-center md:justify-between ">
+          <div className="text-sm md:space-x-4 lg:space-x-24">
             {links.map((link) => (
               <a
                 key={link.id}
@@ -50,6 +73,55 @@ export default function NavBar() {
               change={true}
               variant="py-2.5 px-5 bg-royal-blue text-white hover:bg-white hover:font-bold hover:text-royal-blue"
             />
+          </div>
+        </div>
+
+        {/* Open and close buttons */}
+        <div className="block md:hidden">
+          <button
+            className={`flex items-center absolute top-7 right-0 z-50 py-2 ${
+              isOpen ? "px-6" : "px-4"
+            }`}
+            onClick={() => setOpen(!isOpen)}
+          >
+            <img
+              src={isOpen ? close : open}
+              ref={menuRef}
+              alt="Toggle menu"
+              className={`${isOpen ? "w-8" : "w-10"}`}
+            />
+          </button>
+        </div>
+
+        {/* SIDE-MENU */}
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } bg-dark-700 fixed top-0 right-0 h-screen w-2/3 z-40`}
+        >
+          {/* MOBILE NAV */}
+          <div className="mt-36 pl-6 text-neutral-blue-700 space-y-6">
+            {links.map((link) => (
+              <a
+                key={link.id}
+                href={`#${link.id}`}
+                className={`py-2 block hover:border-b-3 hover:border-white/70 ${
+                  activeSection === link.id ? "border-b-3 border-white" : ""
+                } `}
+                onClick={() => {
+                  return setOpen(false), handleClick(`${link.id}`);
+                }}
+              >
+                {link.text}
+              </a>
+            ))}
+            <a
+              href="#contact"
+              className="inline-block py-2.5 px-5 normal-case bg-royal-blue text-white font-bold rounded-default hover:bg-white hover:font-bold hover:text-royal-blue"
+              onClick={handleLinkClick}
+            >
+              Contact Us
+            </a>
           </div>
         </div>
       </nav>
