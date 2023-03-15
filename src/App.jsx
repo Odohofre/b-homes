5d25ca925311a7eb1271e93a8eb203675048db7f
Odohofre <odohofrebright@gmail.com>
2023-03-15 11:55:12 +0100

    feat: add services component

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import NavBar from "./components/NavBar";
 import Company from "./components/Company";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
+import Services from "./components/Services";
 import Blog from "./components/Blog";
 import Popular from "./components/Popular";
+import FAQ from "./components/Faq";
 
 function App() {
   return (
@@ -13,7 +15,9 @@ function App() {
       <Hero />
       <Company />
       <Popular />
+      <Services />
       <Blog />
+      <FAQ />
       <Testimonials />
       <Footer />
     </>
--- src/components/SectionSubhead.jsx
+++ src/components/SectionSubhead.jsx
@@ -2,7 +2,7 @@ export default function SectionSubhead({ content, variant="", change=false }) {
   return (
     <h2
       className={`text-royal-blue  font-medium ${
-        change == true ? variant : ""
+        change == true ? variant : "text-5xl"
       }`}
     >
       {content}
